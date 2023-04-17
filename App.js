e1375c6a882e72a8c106c2a0cc7496de113d688d
mGunawardhana <mrgunawardhana27368@gmail.com>
2023-04-17 14:25:45 +0530

    adding login page

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
 import Login from './screens/Login';
 
 //screens
@@ -10,12 +10,12 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     flex: 1,
+//     backgroundColor: '#fff',
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//   },
+// });
 //13.28
--- screens/Login.js
+++ screens/Login.js
@@ -1,20 +1,68 @@
 import React from "react";
+import { Formik as Formic } from 'formik';
+import { View } from 'react-native';
 
 import {
     StyledContainer,
     InnerContainer,
-    PageLogo,PageTitle
-} from '../components/styles'
+    PageLogo, PageTitle, SubTitle, StyledFormArea
+    ,LeftIcon,StyledInputLabel,StyledTextInput,RightIcon,Colors
+} from '../components/styles';
+
+//icons
+import {Octicons} from '@expo/vector-icons'
+
+import { StatusBar } from "expo-status-bar";
+const { darkLight, brand, primary } = Colors;
 
 const Login = () => {
     return (
         <StyledContainer>
-            <InnerContainer>
-                <PageLogo resizeMode="cover" style={{ width:280,height:240}} source={require("../assets/img/wine.png")} />
-                <PageTitle>Wine Stores</PageTitle>
+            <StatusBar style="dark" />
+            <InnerContainer style={{justifyContent: 'center',alignItems:'center'}}>
+                <PageLogo resizeMode="cover" style={{ width: 280, height: 240 }} source={require("../assets/img/wine.png")} />
+                <PageTitle >𝖂𝖎𝖓𝖊 𝕾𝖙𝖔𝖗𝖊</PageTitle>
+                <SubTitle>Account Login</SubTitle>
+
+                <Formic
+                    initialValues={{ email: '', password: '' }}
+                    onSubmit={(values) => {
+                        console.log(values);
+                    }}
+                >{({ handleChange, handleBlur, handleSubmit, values }) => (
+                        <StyledFormArea>
+                            <MyTextInput
+                                label="Email Address"
+                                icon="mail"
+                                placeholder="sample@gmail.com"
+                                placeholderTextColor={darkLight}
+                            />
+                            <MyTextInput
+                                label="Email Address"
+                                icon="mail"
+                                placeholder="sample@gmail.com"
+                                placeholderTextColor={darkLight}
+                            />
+                    </StyledFormArea>
+                )}
+
+                </Formic>
+
             </InnerContainer>
         </StyledContainer>
     );
-}
+};
 
+const MyTextInput = ({label, icon, ...props}) => {
+    return (<View style={{}}>
+        <LeftIcon>
+            <Octicons name={ icon} size={30} color={brand}/>
+        </LeftIcon>
+        <StyledInputLabel>
+            {label}
+        </StyledInputLabel>
+        <StyledTextInput {...props} />
+    </View>)
+}
+//19.29
 export default Login;
